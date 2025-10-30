11c3d579c0b874af3781da1616f0fa757d48622e
unlqvt <unlqvt@protonmail.com>
2025-10-30 10:39:46 +0100

    Relocated alphabet files to dedicated project folder

---
+++ SpellCheckingTool/Server/Alphabet/CustomAlphabet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace SpellCheckingTool
+{
+    public class CustomAlphabet : BaseAlphabet
+    {
+        int[] mapping;
+
+        //call the base constructor of the BaseAlphabet class to bundle common logic
+        public CustomAlphabet(char[] chars) : base(chars)
+        {
+            this.mapping = new int[65536];
+
+            //map chars to lookup array for quicker access
+            for (int i = 0; i < chars.Length; ++i)
+                mapping[chars[i]] = i;
+        }
+
+        //Note: non-ASCII characters ('ä', è, î, ...) are scattered all over the place in the unicode table, and searching for the location of each of them in the alphabets
+        //character array is far too expensive (O(n)), therefore this mapping technique is used.
+        //Example: Suppose the character 'ä' is supplied as the 27. letter of the german alphabet.
+        //At the position of 'ä' in the unicode table, 228, which is obtainable via `(int)'ä'`, the 27 gets saved (e.g.: `this.mapping[228] = 27;`).
+        //This way, when searching for 'ä' in the custom alphabets' character table (used when walking the WordTree while performing add/search/delete operations for words), we can simply look
+        //at the position 228 of its mapping table, which stores the position 27, thus creating a feasible O(1) solution.
+        public override int GetCharPositionInArray(char c)
+        {
+            return this.mapping[c];
+        }
+
+    }
+}
---
+++ SpellCheckingTool/Server/Alphabet/LatinAlphabet.cs
@@ -0,0 +1,17 @@
+﻿namespace SpellCheckingTool
+{
+    public class LatinAlphabet : BaseAlphabet
+    {
+        public LatinAlphabet() : base(new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' })
+        {
+            //nothing to do here, everything gets handled by the base class
+        }
+
+        //subtract the offsets of two chars in the ASCII table to get their location in the char array - only works for Latin alphabet, but significantly faster than mapping
+        public override int GetCharPositionInArray(char c)
+        {
+            return (c - 'a');
+        }
+
+    }
+}
