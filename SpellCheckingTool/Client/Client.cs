38b5a0052dcd16c8bc9fc678f14462c14a6ceb69
Paula642 <paula.kropfinger@gmail.com>
2025-11-07 10:44:59 +0100

    Add cmd process integration for command execution

--- SpellCheckingTool/Client/Client.cs
+++ SpellCheckingTool/Client/Client.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 
 namespace SpellCheckingTool
 {
-    public unsafe class Client
+    public class Client
     {
         public static void StartClient(int port)
         {
@@ -19,10 +20,44 @@ namespace SpellCheckingTool
             FilePath filePath = new FilePath(path);
             WordTree tree = new FilePersistenceService().Load(filePath);
 
+            //starting CMD process
+            var p = new Process();
+            p.StartInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            p.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
+            {
+                if (e.Data != null)
+                    Console.WriteLine(e.Data);
+            };
+
+            p.ErrorDataReceived += (object sender, DataReceivedEventArgs e) =>
+            {
+                if (e.Data != null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e.Data);
+                    Console.ResetColor();
+                }
+            };
+
+
+            p.Start();
+            p.StandardInput.AutoFlush = true;
+            p.BeginOutputReadLine();
+            p.BeginErrorReadLine();
+
             Console.WriteLine("Type text and press space to check words.");
 
             StringBuilder currentWord = new StringBuilder();
-            List<string> previousWords = new List<string>();
+            string input = "";
 
             while (true)
             {
@@ -30,36 +65,51 @@ namespace SpellCheckingTool
                 char c = keyInfo.KeyChar;
 
                 //exit
-                //if (c == '!')
-                    //break;
+                if (keyInfo.Key == ConsoleKey.Escape)
+                    break;
+
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                    p.StandardInput.WriteLine(input);
+                    input = "";
+                    currentWord.Clear();
+                    continue;
+                }
 
                 if (keyInfo.Key == ConsoleKey.Backspace)
                 {
-                    if (currentWord.Length > 0)
+                    if (input.Length > 0)
                     {
-                        // remove last character from the word
-                        currentWord.Remove(currentWord.Length - 1, 1);
-
+                        input = input.Substring(0, input.Length - 1);
                         // move cursor one step back, overwrite char with space, move back again
                         Console.Write("\b \b");
-                    }
 
-                    // If Backspace is pressed after a completed word, restore (retype) the previous wordt
-                    else if (previousWords.Count > 0)
-                    {
-                        string lastWord = previousWords.Last();
-                        previousWords.RemoveAt(previousWords.Count - 1);
-                        for (int i = 0; i < lastWord.Length + 1; i++)
-                            Console.Write("\b \b");
+                        if (currentWord.Length > 0)
+                        {
+                            // remove last character from the current word
+                            currentWord.Remove(currentWord.Length - 1, 1);
+                        }
 
-                        currentWord.Append(lastWord);
-                        Console.Write(currentWord);
+                        else
+                        {
+                            // if Backspace is pressed after a completed word, restore the previous word
+                            int lastSpace = input.LastIndexOf(' ');
+                            if (lastSpace >= 0)
+                            {
+                                string lastWord = input.Substring(lastSpace + 1);
+                                currentWord.Clear();
+                                currentWord.Append(lastWord);
+                            }
+                        }
                     }
-                        continue;
+                    continue;
                 }
 
+                input += c;
+
                 // check spelling
-                if (c == ' ' || keyInfo.Key == ConsoleKey.Enter)
+                if (c == ' ')
                 {
                     if (currentWord.Length == 0)
                         continue;
@@ -79,7 +129,6 @@ namespace SpellCheckingTool
                     Console.Write(originalWord + " ");
                     Console.ForegroundColor = ConsoleColor.White;
 
-                    previousWords.Add(originalWord);
                     currentWord.Clear();
                     continue;
                 }
