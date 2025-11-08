0148d17c20b7ffd84e24a9c30da3c33c692ecd07
unlqvt <unlqvt@protonmail.com>
2025-11-08 20:38:38 +0100

    Fixed bug when rerunning GetSuggestions() method

--- SpellCheckingTool/Server/Intellisense/SuggestionService.cs
+++ SpellCheckingTool/Server/Intellisense/SuggestionService.cs
@@ -57,11 +57,19 @@ namespace SpellCheckingTool
             stack[stackIndex++] = tree.rootNode;
             WordTreeNode current = tree.rootNode;
 
+            //the reserved property of each node in the tree gets "modified" on each traversal in order to keep track of all visited nodes.
+            //resetting these to 0 for each node in the tree after each traversal would be too expensive, therefore this approach is employed:
+            //The system has two states: The reserved property of the root node (and therefore all others in the tree) is 0 ("A run"), or it is 'alphabetLength' ("B run")
+            //thse values are used to dynamically flip the bounds of loops etc. depending on whether this traversal is an "A run", or a "B run"
+            int reserved_maxValue = tree.rootNode.reserved == 0 ? alphabetLength : 0;
+            int reserved_incrementValue = tree.rootNode.reserved == 0 ? 1 : -1;
+            int reserved_invertedMaxValue = tree.rootNode.reserved == 0 ? 0 : alphabetLength;
+
             //traverse the tree as long as the stack contains elements
             while (stackIndex > 0)
             {
                 //all childs of this node were visited already, hence it can be popped
-                if (current.reserved == alphabetLength)
+                if (current.reserved == reserved_maxValue)
                 {
                     current = stack[stackIndex - 1];
                     --wordBufferIndex;
@@ -69,31 +77,31 @@ namespace SpellCheckingTool
 
                 //traverse to bottom of tree to get first whole word
                 //used is the bitwise property indicating which childs are in use, hence the value of the used property will be zero for leaf nodes without children 
-                while (current.reserved < alphabetLength)
+                while (current.reserved != reserved_maxValue)
                 {
                     //find smallest child of node which is yet to be serialized and save its index in the reserved property of the node
-                    for (int characterIndex = current.reserved; characterIndex < alphabetLength; ++characterIndex)
-                        if (current.Nodes[characterIndex] != null)
+                    for (int characterIndex = current.reserved; characterIndex != reserved_maxValue; characterIndex += reserved_incrementValue)
+                        if (current.Nodes[(characterIndex - reserved_invertedMaxValue) * reserved_incrementValue] != null)
                         {
                             //the leftmost non-zero child pointer is traversed in order to find its children and pushed on the stack for backtracing
-                            stack[stackIndex++] = current.Nodes[characterIndex];
-                            wordBuffer[wordBufferIndex++] = alphabetChars[characterIndex];
+                            stack[stackIndex++] = current.Nodes[(characterIndex - reserved_invertedMaxValue) * reserved_incrementValue];
+                            wordBuffer[wordBufferIndex++] = alphabetChars[(characterIndex - reserved_invertedMaxValue) * reserved_incrementValue];
 
                             //examine the next child on future visits, as this one is non-null
-                            current.reserved = characterIndex + 1;
+                            current.reserved = characterIndex + reserved_incrementValue;
 
                             //take a look at the leftmost child of this node and interrupt the traversal for this one
                             //the index of the first child node after this one that has yet to be checked is stored in the 'reserved' property (may be null))
-                            current = current.Nodes[characterIndex];
+                            current = current.Nodes[(characterIndex - reserved_invertedMaxValue) * reserved_incrementValue];
                             break;
                         }
                         else
                             //mark note as 'traversed', as it is null and contains no children
-                            current.reserved = characterIndex + 1;
+                            current.reserved = characterIndex + reserved_incrementValue;
                 }
 
                 //check if nodes where all child nodes have been traversed represent a word themselves; if not, pop it from the stack and resume with the next node
-                if (current.reserved == alphabetLength && current.IsWord != true)
+                if (current.reserved == reserved_maxValue && current.IsWord != true)
                 {
                     stackIndex--;
                     continue;
--- TestProject/Unit/SuggestionTests.cs
+++ TestProject/Unit/SuggestionTests.cs
@@ -12,15 +12,16 @@ namespace TestProject.Unit
         [TestInitialize]
         public void SetupTests()
         {
-            IAlphabet alphabet = new LatinAlphabet();
-            Word[] words = Word.ParseWords(alphabet, new string[] { "these", "are", "some", "random", "english", "words", "containing", "only", "latin", "characters" });
-            tree = new WordTree(alphabet, new FilePersistenceService());
-            tree.Add(words);
+            
         }
 
         [TestMethod]
         public void GetHundredSuggestionsOfAllDistances_ShouldReturnTenSuggestions()
         {
+            IAlphabet alphabet = new LatinAlphabet();
+            Word[] words = Word.ParseWords(alphabet, new string[] { "these", "are", "some", "random", "english", "words", "containing", "only", "latin", "characters" });
+            tree = new WordTree(alphabet, new FilePersistenceService());
+            tree.Add(words);
             SuggestionResult result = tree.GetSuggestions("containing", 100, 999);
             Assert.AreEqual(result.GetSuggestionCount(), tree.metaData.wordCount);
         }
