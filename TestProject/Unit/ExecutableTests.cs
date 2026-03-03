ae5a93124088270cbde57db7ba3a9e0cb5871121
unlqvt <unlqvt@protonmail.com>
2026-03-03 14:54:23 +0100

    test: Added cross-platform executable test

---
+++ TestProject/Unit/ExecutableTests.cs
@@ -0,0 +1,23 @@
+﻿using SpellCheckingTool.Infrastructure.Executables;
+using System.Runtime.InteropServices;
+
+namespace TestProject.Unit;
+
+[TestClass]
+public class ExecutableTests
+{
+    [TestMethod]
+    public void GetExecutableSuggestionsOnCurrentPlatform_ShouldContainApplicationsInstalledOnSystem()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            var executables = new WindowsExecutableParser().GetAllShellExecutables();
+            Assert.IsTrue(executables.Contains("powershell"));
+        }
+        else
+        {
+            var executables = new LinuxExecutableParser().GetAllShellExecutables();
+            Assert.IsTrue(executables.Contains("bash"));
+        }
+    }
+}
