e009fc107f6781f45d8e86559b4a4742d3b3a301
SafaeKa <safae.kartite@nttdata.com>
2026-03-09 14:00:48 +0100

    deleted unnecessary file

--- SpellCheckingTool.Presentation/ConsoleClient/UserWordDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SpellCheckingTool.Presentation.ConsoleClient;
-public class UserWordDto
-{
-    public string Word { get; set; } = "";
-    public int UsageCount { get; set; }
-    public DateTime LastUsedAt { get; set; }
-}
