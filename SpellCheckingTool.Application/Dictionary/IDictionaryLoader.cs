df20e137ed4faf79d93fefabe12ffab0e30122a4
Paula642 <paula.kropfinger@gmail.com>
2026-04-28 22:05:38 +0200

    fixed DictionaryLoader naming mix-up

--- SpellCheckingTool.Application/Dictionary/IDictionaryLoader.cs
+++ SpellCheckingTool.Application/Dictionary/IDictionaryLoader.cs
@@ -3,17 +3,7 @@ using SpellCheckingTool.Domain;
 
 namespace SpellCheckingTool.Application.Dictionary;
 
-public class DictionaryLoader : IDictionaryLoader
+public interface IDictionaryLoader
 {
-    private readonly IPersistenceService persistenceService;
-
-    public DictionaryLoader(IPersistenceService persistenceService)
-    {
-        this.persistenceService = persistenceService;
-    }
-
-    public IWordStorage Load(FilePath filepath)
-    {
-        return persistenceService.Load(filepath);
-    }
+    IWordStorage Load(FilePath filepath);
 }
